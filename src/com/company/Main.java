831a827859ccdfce1eb78aea22798ddf76e22b7e
Yuriy108 <sef.yuriy@gmail.com>
2020-11-30 13:38:26 +0200

    two

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,13 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Swallow sw=new Swallow("Red",50);
-        Swallow sw1= new Swallow("Green",60);
-
-        Bird b=new Bird();
-        System.out.println(sw);
-        System.out.println(sw1);
-
+        System.out.println("hello world");
 
 
 
